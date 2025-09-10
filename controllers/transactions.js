7771d7a7cbebb4e40221fa0213122722e412c175
Tharinda Pathirana <tharindapathirana27@gmail.com>
2025-09-11 00:02:24 +0530

    add routes

---
+++ controllers/transactions.js
@@ -0,0 +1,22 @@
+// @desc  Get all transactions
+// @route GET /api/v1/transactions
+// @access Public
+exports.getTransactions = (req, res,next) => {
+    res.send('GET Transactions');
+    }
+
+
+// @desc  Add all transactions
+// @route POST /api/v1/transactions
+// @access Public
+exports.addTransactions = (req, res,next) => {
+    res.send('POST Transactions');
+    }
+    
+    
+// @desc  Delete all transactions
+// @route DELETE /api/v1/transactions
+// @access Public
+exports.deleteTransactions = (req, res,next) => {
+    res.send('DELETE Transactions');
+    }
